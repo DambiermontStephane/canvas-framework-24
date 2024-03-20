d5ce3aaf3d574b60a6404187a0ef262c464b527c
DambiermontStephane <stephane.dambiermont@student.hepl.be>
2024-03-20 12:21:49 +0100

    changes circle class

---
+++ src/ts/shapes/Circle.ts
@@ -0,0 +1,20 @@
+import {Shape} from "./Shape";
+import {IColor} from "../types/IColor";
+
+export class Circle extends Shape{
+    private radius:number;
+
+    constructor(ctx: CanvasRenderingContext2D, x: number, y: number, radius: number, color: IColor) {
+        super(ctx, x, y, color);
+        this.radius = radius;
+    }
+
+    public draw() {
+        this.ctx.save();
+        this.ctx.fillStyle = this.color.toString();
+        this.ctx.beginPath()
+        this.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        this.ctx.fill()
+        this.ctx.restore();
+    }
+}
